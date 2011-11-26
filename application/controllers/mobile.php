44422a5a46c77cbce5873bccc5d0af6309eeebf7
Christian Marth <christian.marth@cloudgroup.com.au>
2011-11-26 16:40:31 +1100

    changes

--- application/controllers/mobile.php
+++ application/controllers/mobile.php
@@ -1,8 +1,18 @@
 <?php
-Class mobile extends CI_Controller{
-    
-    function index(){
-        print_r($this->input->post());
+
+Class mobile extends CI_Controller {
+
+    function index() {
+        $card = array(
+        'name' => $this->input->post('name'),
+        'number' => $this->input->post('number'),
+        'expiry' => $this->input->post('expiry')
+        );
+        
+        print_r($card);
+        
     }
+
 }
+
 ?>
