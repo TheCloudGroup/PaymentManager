2a733451331590372a348d84465f21b0b2c40256
Christian Marth <christian.marth@cloudgroup.com.au>
2011-11-26 16:29:43 +1100

    added sparks

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,7 @@ $root = "http://".$_SERVER['HTTP_HOST'];
 $root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 
 $config['base_url']    = "$root"; 
-  
+ 
 //$config['base_url']	= "http://smashingsites.com.au/invoicemanager/";
 
 /*
@@ -28,7 +28,7 @@ $config['base_url']    = "$root";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "index.php";
+$config['index_page'] = "";
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/mobile.php
@@ -0,0 +1,8 @@
+<?php
+Class mobile extends CI_Controller{
+    
+    function index(){
+        $this->input->post();
+    }
+}
+?>
