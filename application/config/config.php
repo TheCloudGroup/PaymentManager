f73cdc5bf6e48341308e5163a2e2cd1ad53d923d
Christian Marth <christian.marth@cloudgroup.com.au>
2011-11-29 11:39:28 +1100

    Update application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -28,7 +28,7 @@ $config['base_url']    = "$root";
 | variable so that it is blank.
 |
 */
-$config['index_page'] = "";
+$config['index_page'] = "index.php";
 
 /*
 |--------------------------------------------------------------------------
